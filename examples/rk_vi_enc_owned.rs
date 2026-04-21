712c780570c0118e1737f578bebd4427d7b8a33a
Alexander Koval <kovalidis@gmail.com>
2026-04-21 19:02:07 +0300

    PoC for owned API

---
+++ examples/rk_vi_enc_owned.rs
@@ -0,0 +1,132 @@
+use std::fs::File;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::time::Duration;
+
+use argh::{FromArgs, FromArgValue};
+use rusty_rockit::RockitSys;
+use rusty_rockit::aiq::AiqContext;
+use rusty_rockit::venc::{
+    self, Codec, H26xRateControl, H264Profile, HevcProfile, VencChannelOwned, VencConfig,
+};
+
+/// Test rockchip encoder
+#[derive(Debug, FromArgs)]
+pub struct Args {
+    /// camera id
+    #[argh(option, short = 'c', default = "0")]
+    camera_id: u8,
+    /// logging system
+    #[argh(option, short = 'e', default = "CodecKind::H264")]
+    encoder: CodecKind,
+    /// output file
+    #[argh(option, short = 'o')]
+    output_file: Option<PathBuf>,
+}
+
+#[derive(Debug, FromArgValue)]
+enum CodecKind {
+    H264,
+    Hevc,
+}
+
+struct Encoder {
+    enc_channel: VencChannelOwned<venc::state::Started>,
+}
+
+impl Encoder {
+    fn new(
+        mpi: &RockitSys,
+        camera_id: u8,
+        codec: CodecKind,
+        width: u16,
+        height: u16,
+    ) -> Result<Self, rusty_rockit::Error> {
+        let codec = match codec {
+            CodecKind::H264 => Codec::H264 {
+                rate_control: H26xRateControl::Cbr {
+                    gop: 30,
+                    framerate: 30,
+                    bitrate_kbps: 4 * 1024,
+                },
+                profile: H264Profile::High,
+            },
+            CodecKind::Hevc => Codec::Hevc {
+                rate_control: H26xRateControl::Cbr {
+                    gop: 30,
+                    framerate: 30,
+                    bitrate_kbps: 4 * 1024,
+                },
+                profile: HevcProfile::Main,
+            },
+        };
+
+        let cam = mpi.camera(camera_id, 1).expect("Camera device").into_owned();
+
+        let pipe = cam.get_pipe(0).expect("Rockit pipe");
+        let channel = pipe.create_channel(0, width, height).expect("Rockit channel");
+
+        let enc_channel = mpi.encoder(
+            0,
+            &VencConfig {
+                width,
+                height,
+                codec,
+                buf_count: 2,
+            }
+        )
+            .expect("Encoder channel")
+            .into_owned();
+        let enc_channel = enc_channel.start().expect("Encoder start");
+        Ok(Self { enc_channel })
+    }
+
+    fn process_frame(&self) {
+        
+    }
+}
+
+fn main() {
+    env_logger::init();
+
+    let args: Args = argh::from_env();
+
+    let camera_id = args.camera_id;
+    let width = 1920;
+    let height = 1080;
+
+
+    let output_filename = args.output_file.as_deref()
+        .unwrap_or_else(|| match args.encoder {
+            CodecKind::H264 => Path::new("test-stream.h264"),
+            CodecKind::Hevc => Path::new("test-stream.hevc"),
+        });
+
+    log::info!("Starting AIQ...");
+    let aiq_ctx = AiqContext::init(camera_id, None).expect("AIQ context");
+    let _aiq_ctx = aiq_ctx.start().expect("AIQ start");
+
+    log::info!("Creating MPI context...");
+    let rockit_sys = RockitSys::init().expect("Rockit");
+    let encoder = Encoder::new(&rockit_sys, camera_id, args.encoder, width, height);
+    let mut file = File::create(output_filename).expect("Create file");
+    for i in 0..30 {
+        encoder.process_frame().expect("Process frame");
+    }
+    // {
+    //     let enc = enc_channel.bind(&channel).expect("Bind encoder");
+    //     let mut frame = enc_channel.alloc_frame();
+
+
+    //     for i in 0..30 {
+    //         let stream = enc_channel.get_stream(&mut frame, Duration::from_millis(100))
+    //             .expect("Encoder stream");
+    //         let packet_data = stream.data().expect("Packet data");
+    //         println!("{}: Packet len: {}", i + 1, packet_data.len());
+
+    //         file.write_all(packet_data).expect("Write file");
+
+    //         std::thread::sleep(std::time::Duration::from_millis(30));
+    //     }
+    // }
+}
